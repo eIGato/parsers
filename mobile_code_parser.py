15e0524fbe6fb1e4436a4cea0090db4f8acc411c
Evgeny Denisov <denisov.evgeny@220-volt.ru>
2017-04-22 18:27:40 +0300

    Optimized parsing and progress message. Parametrized output.

--- mobile_code_parser.py
+++ mobile_code_parser.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
-# vim :set fileencoding=utf-8:
+#!.venv/bin/python3
+# vim: set fileencoding=utf-8 :
 
-from sys import stderr
+from sys import stderr, stdout, argv
+from io import BytesIO
+
 from requests import get
 from lxml import html
 
@@ -9,7 +11,7 @@ from lxml import html
 def get_full_table():
     raw_data = list()
     for code in range(900, 1000):
-        print(code, file=stderr)
+        print('Parsing code {}...'.format(code), end='\r', file=stderr)
         spam = get_part('http://www.kody.su/mobile/{}'.format(code))
         if spam != 404:
             raw_data.extend(spam)
@@ -17,9 +19,10 @@ def get_full_table():
 
 
 def get_part(url):
-    if get(url).status_code == 404:
+    response = get(url)
+    if response.status_code == 404:
         return 404
-    htm = html.parse(url).getroot().find_class('tbnum').pop()
+    htm = html.parse(BytesIO(response.content)).getroot().find_class('tbnum').pop()
     data = [get_row(_) for _ in htm.getchildren()[1:]]
     return data
 
@@ -29,19 +32,20 @@ def get_row(html_row):
     row = html_row.getchildren()
     long_template = row[0].text.split('-')
     result['code'] = long_template[0]
-    result['templates'] = list((long_template[1],))
-    for span in row[0].getchildren():
-        if span.text:
-            result['templates'].append(span.text)
+    result['templates'] = [long_template[1]]
+    result['templates'].extend([span.text for span in row[0].getchildren() if span.text])
     result['provider'] = row[1].text
-    result['region'] = row[2].text
-    if not result['region']:
-        result['region'] = ''
+    result['region'] = row[2].text or ''
     return result
 
 
-if __name__ == '__main__':
+def parse(out):
     raw_table = get_full_table()
+    file = open(out, 'w') if type(out) is str else out
     for row in raw_table:
         for template in row['templates']:
-            print('"{}{}";"{}";"{}"'.format(row['code'], template, row['provider'], row['region']))
+            print('"{}{}";"{}";"{}"'.format(row['code'], template, row['provider'], row['region']), file=file)
+
+
+if __name__ == '__main__':
+    parse(argv[1] if len(argv) > 1 else stdout)
